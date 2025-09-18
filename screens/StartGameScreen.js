3dbb68584f98de903f1218f56628a9ecc25667ec
Nataliachm <nataliachm97@gmail.com>
2025-09-18 15:49:05 -0500

    Initialize React Native project with Expo and create initial screens
    - Added package.json for project configuration and dependencies.
    - Created StartGameScreen with basic layout and buttons.
    - Added placeholder files for GameOverScreen and GameScreen.

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,24 @@
+import { StyleSheet, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+  },
+});
